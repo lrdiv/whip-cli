bd67f52d53bb84cb2cf9673b718edba1d07fec4d
Lawrence Davis <lawrence@lrdiv.co>
2022-08-22 09:14:50 -0500

    Rewrite in go with bubbletea

--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module whip
 go 1.16
 
 require (
-	github.com/charmbracelet/bubbles v0.13.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/antchfx/htmlquery v1.2.5 // indirect
+	github.com/antchfx/xmlquery v1.3.12 // indirect
+	github.com/atotto/clipboard v0.1.4
+	github.com/charmbracelet/bubbles v0.13.0
 	github.com/charmbracelet/bubbletea v0.22.0
+	github.com/charmbracelet/lipgloss v0.5.0
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 )
--- whip.go
+++ whip.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -9,82 +10,201 @@ import (
 	"os"
 	"strings"
 
+	"github.com/atotto/clipboard"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/gocolly/colly"
 )
 
-var platforms = []string{
-	"spotify",
-	"itunes",
-	"youtube",
-	"tidal",
-	"amazonMusic",
-	"pandora",
-	"deezer",
-	"audiomack",
-	"qobuz",
+// Enum for managing TUI state
+type state int64
+const (
+	GetOriginalUrl state = iota
+	ChoosePlatform
+	FetchSongwhip
+	CrawlSongwhip
+	Done
+	HasError
+)
+
+// Message types to be used in bubbletea commands
+type errorMsg struct{}
+type tickMsg struct{}
+type httpReadyMsg struct{}
+type httpCallbackMsg struct {
+	url string
+}
+type songwhipCrawlMsg struct {
+	url string
 }
 
-const (
-	getSource State = 0
-	choosePlatform State = 1
-	fetching State = 2
-	done State = 3
-	hasError State = 4
-)
 
-type State int
+type platform struct {
+	Slug string
+	Title string
+	HelpText string
+}
+
+var platforms [10]platform = [10]platform{
+	 {
+		Slug: "songwhip",
+		Title: "Songwhip",
+		HelpText: "Get a Songwhip URL with links to all available platforms.",
+	}, {
+		Slug: "spotify",
+		Title: "Spotify",
+	}, {
+		Slug: "itunes",
+		Title: "Apple Music",
+	}, {
+		Slug: "youtube",
+		Title: "YouTube Music",
+	}, {
+		Slug: "tidal",
+		Title: "Tidal",
+	}, {
+		Slug: "amazonMusic",
+		Title: "Amazon Music",
+	}, {
+		Slug: "pandora",
+		Title: "Pandora",
+	}, {
+		Slug: "deezer",
+		Title: "Deezer",
+	}, {
+		Slug: "audiomack",
+		Title: "AudioMack",
+	}, {
+		Slug: "qobuz",
+		Title: "Qobuz",
+	},
+}
 
 type model struct {
-	state State
-	source textinput.Model
-	cursor int
-	platform int
-	url string
-	errMsg error
-	logFile *os.File
+	State state
+	OriginalUrl textinput.Model
+	PlatformCursor int
+	Platform platform
+	SongwhipData songwhipResponse
+	PlatformUrl string
+	Log *os.File
 }
-
-type httpCallbackMsg string
 
 func main() {
 	model := initialModel()
-	logFile, err := openLogFile("./tmp.log")
-	model.logFile = logFile
-	if err != nil {
-		fmt.Print("Ah shit a log error")
-	}
-	
 	p := tea.NewProgram(model)
+
 	if err := p.Start(); err != nil {
 		fmt.Printf("Oh no! An error! :( %v", err)
 		os.Exit(1)
 	}
 }
 
-func openLogFile(filename string) (*os.File, error) {
-	logFile, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
-	if err != nil {
-		return nil, err
+func initialModel() model {
+	originalUrlInput := textinput.New()
+	originalUrlInput.Placeholder = "https://open.spotify.com/track/4uLU6hMCjMI75M1A2tKUQC"
+	originalUrlInput.Focus()
+
+	return model{
+		State: GetOriginalUrl,
+		OriginalUrl: originalUrlInput,
+		PlatformCursor: 0,
+		PlatformUrl: "",
+		Log: openLogFile("./tmp/whip.log"),
 	}
-	return logFile, nil
 }
 
-func (m model) logText(text string) {
-	logger := log.New(m.logFile, "[info]", log.LstdFlags|log.Lshortfile|log.Lmicroseconds)
+func openLogFile(path string) (*os.File)  {
+	os.Truncate("./tmp/whip.log", 0)
+	logFile, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil || logFile == nil {
+		return nil
+	}
+	return logFile
+}
+
+func (m model) writeToLog(text string) {
+	logger := log.New(m.Log, "[info]", log.LstdFlags|log.Lmicroseconds)
 	logger.Println(text)
 }
 
-func initialModel() model {
-	ti := textinput.New()
-	ti.Placeholder = "https://open.spotify.com/track/Sfsdj3924hjd"
-	ti.Focus()
-	return model{
-		state: getSource,
-		source: ti,
-		cursor: 0,
-		platform: 0,
-		url: "",
+func quit(log *os.File) tea.Cmd {
+	if log != nil {
+		defer log.Close()
+	}
+	return tea.Quit
+}
+
+func platformSelectionView(platformCursor int) string {
+	var sb strings.Builder
+	sb.WriteString("Which platform do you want a link for?\n\n")
+	for i, platform := range platforms {
+		cursor := " "
+		if platformCursor == i {
+			cursor = ">"
+			style := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#00FF00"))
+			sb.WriteString(style.Render(fmt.Sprintf("%s %s", cursor, platform.Title)))
+			if platform.HelpText != "" {
+				sb.WriteString(fmt.Sprintf(": %s", platform.HelpText))
+			}
+		} else {
+			sb.WriteString(fmt.Sprintf("%s %s", cursor, platform.Title))
+		}
+		sb.WriteString("\n")
+	}
+	sb.WriteString("\nPress ctrl+c or esc to quit.\n")
+	return sb.String()
+}
+
+func getSongwhipData(url string) tea.Cmd {
+	return func() tea.Msg {
+		var songwhipData songwhipResponse
+		var jsonData = bytes.NewBuffer([]byte(fmt.Sprintf(`{"url": "%s"}`, url)))
+		res, err := http.Post("https://songwhip.com/", "application/json", jsonData)
+
+		if err != nil {
+			return errorMsg{}
+		}
+
+		defer res.Body.Close()
+
+		if res.StatusCode == http.StatusOK {
+			bodyBytes, err := io.ReadAll(res.Body)
+			if err != nil {
+				return errorMsg{}
+			}
+
+			jsonErr := json.Unmarshal(bodyBytes, &songwhipData)
+			if jsonErr != nil {
+				return errorMsg{}
+			}
+			return httpCallbackMsg{
+				url: songwhipData.Url,
+			}
+		}
+
+		return errorMsg{}
+	}
+}
+
+func crawlSongwhip(url string, platform string) tea.Cmd {
+	return func() tea.Msg {
+		var platformUrl string
+
+		if platform == "songwhip" {
+			return songwhipCrawlMsg{ url: url }
+		}
+
+		c := colly.NewCollector(colly.AllowedDomains("songwhip.com"))
+		selector := fmt.Sprintf("a[data-testid=\"ServiceButton %s itemLinkButton %sItemLinkButton\"]", platform, platform)
+
+		c.OnHTML(selector, func (e *colly.HTMLElement) {
+			platformUrl = e.Attr("href")
+		})
+
+		c.Visit(url)
+		return songwhipCrawlMsg{ url: platformUrl }
 	}
 }
 
@@ -92,143 +212,81 @@ func (m model) Init() tea.Cmd {
 	return textinput.Blink
 }
 
-func (m *model) updatePlatform(msg tea.Msg) (tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "up":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "down":
-			if m.cursor < len(platforms)-1 {
-				m.cursor++
-			}
-		case "enter":
-			m.state = fetching
-			m.platform = m.cursor
-		}
-	}
-
-	return nil
-}
-
-func (m *model) updateSource(msg tea.Msg) (tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEnter:
-			m.state = choosePlatform
-			return nil
-
-		case tea.KeyCtrlC, tea.KeyEsc:
-			return tea.Quit
-		}
-
-	case error:
-		m.errMsg = msg
-		return nil
-	}
-
-	m.source, cmd = m.source.Update(msg)
-	return cmd
-}
-
-func (m *model) getUrl() (tea.Cmd) {
-	m.logText("About to make request...")
-
-	var jsonData = []byte(`{
-			"url": {{.m.source}}
-		}`)
-	client := http.Client{}
-	req, _ := http.NewRequest("POST", "https://songwhip.com/", bytes.NewBuffer(jsonData))
-	req.Header = http.Header{
-		"Content-Type": {"application/json"},
-	}
-	m.logText("Request prepared...")
-	res, err := client.Do(req)
-	if err != nil {
-		m.logText("We got error :( :(")
-		m.state = hasError
-		log.Fatal(err)
-		os.Exit(1)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode == http.StatusOK {
-		bodyBytes, err := io.ReadAll(res.Body)
-		if err != nil {
-			log.Fatal(err)
-			m.state = hasError
-		}
-		m.Update(httpCallbackMsg(string(bodyBytes)))
-	}
-	
-	return nil
-}
-
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var ret tea.Cmd
-
-	var keyMsg, isKeyMsg = msg.(tea.KeyMsg)
-	if (isKeyMsg && keyMsg.Type == tea.KeyCtrlC || keyMsg.Type == tea.KeyEsc) {
-		return m, tea.Quit
-	}
-
-	switch m.state {
-	case getSource:
-		ret = m.updateSource(msg)
-	case choosePlatform:
-		ret = m.updatePlatform(msg)
-	case fetching:
-		m.getUrl()
-		switch msg := msg.(type) {
-		case httpCallbackMsg:
-			m.state = done
-			m.url = string(msg)
+	// Keypress messages can come from either the GetOriginalUrl or ChoosePlatform states so we handle those before
+	// focusing on custom message structs
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if m.State == Done {
+			return m, quit(m.Log)
 		}
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, quit(m.Log)
+		case tea.KeyEnter:
+			switch m.State {
+			case GetOriginalUrl:
+				m.State = ChoosePlatform
+				return m, nil
+			case ChoosePlatform:
+				m.Platform = platforms[m.PlatformCursor]
+				m.State = FetchSongwhip
+				return m.Update(httpReadyMsg{})
+			}
+		case tea.KeyUp:
+			if m.State == ChoosePlatform && m.PlatformCursor > 0 {
+				m.PlatformCursor--
+			}
+		case tea.KeyDown:
+			if m.State == ChoosePlatform && m.PlatformCursor < len(platforms) - 1 {
+				m.PlatformCursor++
+			}
+		default:
+			if m.State == GetOriginalUrl {
+				originalUrl, cmd := m.OriginalUrl.Update(msg)
+				m.OriginalUrl = originalUrl
+				return m, cmd
+			}
+		}
+	case httpReadyMsg:
+		return m, getSongwhipData(m.OriginalUrl.Value())
+	case httpCallbackMsg:
+		m.State = CrawlSongwhip
+		return m, crawlSongwhip(msg.url, m.Platform.Slug)
+	case songwhipCrawlMsg:
+		clipboard.WriteAll(msg.url)
+		m.PlatformUrl = msg.url
+		m.State = Done
+	case errorMsg:
+		m.State = HasError
 	}
-
-	return m, ret
+	return m, nil
 }
 
 func (m model) View() string {
-	s := ""
-
-	if (m.state == getSource) {
+	switch m.State {
+	case HasError:
+		return "Uh oh! We've encountered an error :("
+	case GetOriginalUrl:
 		return fmt.Sprintf("Enter a track or album URL from any supported platform...\n\n%s\n\n%s",
-			m.source.View(),
-			"(esc to quit)",
+			m.OriginalUrl.View(),
+			"(ctrl+c or esc to quit)",
 		)
-	}
-
-	if (m.state == choosePlatform) {
-		s += "Which platform do you want a link for?\n\n"
-		for i, platform := range platforms {
-			cursor := " "
-			if m.cursor == i {
-				cursor = ">"
-			}
-
-			s += fmt.Sprintf("%s %s\n", cursor, strings.Title(platform))
+	case ChoosePlatform:
+		return platformSelectionView(m.PlatformCursor)
+	case FetchSongwhip, CrawlSongwhip:
+		return "Fetching Songwhip Data..."
+	case Done:
+		if len(m.PlatformUrl) == 0 {
+			return fmt.Sprintf("Oh no! Could not find a URL for %s :(", m.Platform.Title)
+		} else {
+			return fmt.Sprintf(
+				"Here's your %s URL! The link has been copied to your clipboard.\n\n%s\n\n(press any key to quit)",
+				m.Platform.Title,
+				m.PlatformUrl,
+			)
 		}
-
-		s += "\nPress q to quit.\n"
 	}
 
-	if (m.state == fetching) {
-		s += "Fetching links from Songwhip..."
-	}
-
-	if (m.state == done) {
-		s += m.url
-	}
-
-	if (m.state == hasError) {
-		s += "aw fudge we got error."
-	}
-
-	return s
+	return ""
 }
